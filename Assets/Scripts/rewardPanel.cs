4827b9abdc6fdf867821b7d92f52ee024e901bf1
HamzaAziz786 <98694278+HamzaAziz786@users.noreply.github.com>
2023-08-26 08:22:57 -0700

    asdasddsa

---
+++ Assets/Scripts/rewardPanel.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using DG.Tweening;
+using System;
+using TMPro;
+
+public class rewardPanel : MonoBehaviour
+{
+    public List<float> positions;
+    public List<int> Reward;
+   
+    int currentindex=0;
+    public GameObject Roller;
+    public TMP_Text rewardamount;
+    private void Start()
+    {
+        InvokeRepeating(nameof(Scroller), .2f, .2f);
+    }
+
+    public void Scroller()
+    {
+
+        Roller.transform.DOLocalMoveX(positions[currentindex], .2f);
+        currentindex++;
+
+        if (currentindex >= positions.Count)
+            currentindex = 0;
+
+        rewardamount.text = Reward[currentindex].ToString();
+    }
+
+    public void OnClickReward()
+    {
+        MyAdmobAds_Manager.Instance.ShowRewardBasedVideo();
+        CancelInvoke(nameof(Scroller));
+       
+        Debug.Log(Reward[currentindex]);
+    }
+    public void Continue()
+    {
+        InvokeRepeating(nameof(Scroller), .2f, .2f);
+    }
+
+}
